593b7654cc9d7af8576d8171f379ac04ac57b8a0
DenisTDR <denis.tdr@gmail.com>
2017-05-26 23:42:03 +0300

    frontend: cuipasa-ed

--- frontend/src/app/cuipasa.component.ts
+++ frontend/src/app/cuipasa.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './cuipasa.component.html',
+  styleUrls: ['./cuipasa.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
--- frontend/src/app/cuipasa.module.ts
+++ frontend/src/app/cuipasa.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './cuipasa.component';
 
 @NgModule({
   declarations: [
@@ -17,4 +17,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class CuipasaModule { }
