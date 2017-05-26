6c60b541eb1984ceba6e35da45629de6d8f5c3d8
DenisTDR <denis.tdr@gmail.com>
2017-05-27 02:03:29 +0300

    frontend: mdbootstrap-ed

--- frontend/src/app/modules/cuipasa/cuipasa.module.ts
+++ frontend/src/app/modules/cuipasa/cuipasa.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomePageComponent} from './pages/home/home-page.component';
 import {ContactPageComponent} from './pages/contact-page/contact-page.component';
+import {ApiService} from './services/api.service';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import {ContactPageComponent} from './pages/contact-page/contact-page.component'
     HomePageComponent,
     ContactPageComponent
   ],
-  exports: [HomePageComponent]
+  exports: [HomePageComponent],
+  providers: [ApiService]
 })
 export class CuipasaModule {
 }
