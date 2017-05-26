0bf19fcc1b0e97f96bbcf067828774717f6e40e0
DenisTDR <denis.tdr@gmail.com>
2017-05-26 23:25:02 +0300

    frontend - new project

---
+++ frontend/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Hacktm2017FrontendPage } from './app.po';
+
+describe('hacktm2017-frontend App', () => {
+  let page: Hacktm2017FrontendPage;
+
+  beforeEach(() => {
+    page = new Hacktm2017FrontendPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
