128ddc1fbf3a05840ba9a0d9f451f3c13526e253
DenisTDR <denis.tdr@gmail.com>
2017-05-27 23:51:15 +0300

    ngx, modal?

--- frontend/src/app/modules/cuipasa/services/static-info.service.ts
+++ frontend/src/app/modules/cuipasa/services/static-info.service.ts
@@ -1,17 +1,27 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
 import {Service} from '../models/service';
 import {MockStaticInfoService} from './mock-static-info.service';
 import {Characteristic} from '../models/characteristic';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class StaticInfoService {
 
-  constructor(private mockStaticService: MockStaticInfoService) {
+  private backendUri = 'http://api.hacktm.tdrs.me';
+
+  constructor(private mockStaticService: MockStaticInfoService,
+              private http: Http) {
   }
 
-  public getServices(): Observable<Service[]> {
-    return this.mockStaticService.getServices();
+  public getServices(): any {
+    const uri = `${this.backendUri}/get/services`;
+    return this.http.get(uri).map((resp: any) => {
+        return resp.json();
+      }
+    );
+    // return this.mockStaticService.getServices();
   }
 
   public getServiceCharacteristics(serviceId: number): Observable<Characteristic[]> {
